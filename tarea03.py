a71887afc9f0b7b0bed1c353485352362d3432be
Rafael F. Percca Trejo <perccatrejorafael@gmail.com>
2023-05-30 19:52:27 -0500

    cursor

--- tarea03.py
+++ tarea03.py
@@ -18,31 +18,31 @@ def get_areas():
     """
     Devuelve todas las áreas.
     """
-    data = conexion.cursor()
-    data.execute("SELECT * FROM areas")
-    areas = data.fetchall()
-    data.close()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM areas")
+    areas = cursor.fetchall()
+    cursor.close()
     return jsonify(areas)
 
 # Endpoint para crear un área
 @app.route('/area', methods=['POST'])
 def create_area():
     new_area = request.get_json()
-    data = conexion.cursor()
-    data.execute("INSERT INTO areas (id, nombre, piso) VALUES (%s, %s, %s) RETURNING *",
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO areas (id, nombre, piso) VALUES (%s, %s, %s) RETURNING *",
                    (new_area["id"], new_area["nombre"], new_area["piso"]))
-    created_area = data.fetchone()
+    created_area = cursor.fetchone()
     conexion.commit()
-    data.close()
+    cursor.close()
     return jsonify(created_area), 201
 
 # Endpoint para obtener un área por su ID
 @app.route('/area/<int:area_id>', methods=['GET'])
 def get_area(area_id):
-    data = conexion.cursor()
-    data.execute("SELECT * FROM areas WHERE id = %s", (area_id,))
-    area = data.fetchone()
-    data.close()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM areas WHERE id = %s", (area_id,))
+    area = cursor.fetchone()
+    cursor.close()
 
     if area:
         return jsonify(area)
@@ -53,12 +53,12 @@ def get_area(area_id):
 @app.route('/empleado', methods=['POST'])
 def create_empleado():
     new_empleado = request.get_json()
-    data = conexion.cursor()
-    data.execute("INSERT INTO empleados (id, nombre, apellido, correo, piso, area_id) VALUES (%s, %s, %s, %s,%s, %s) RETURNING *",
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO empleados (id, nombre, apellido, correo, piso, area_id) VALUES (%s, %s, %s, %s,%s, %s) RETURNING *",
                    (new_empleado["id"], new_empleado["nombre"], new_empleado["apellido"], new_empleado["correo"], new_empleado["piso"],new_empleado["area_id"]))
-    created_empleado = data.fetchone()
+    created_empleado = cursor.fetchone()
     conexion.commit()
-    data.close()
+    cursor.close()
     return jsonify(created_empleado), 201
 
 # Endpoint para obtener empleados de según condición de query
@@ -67,16 +67,16 @@ def get_empleados():
     email = request.args.get('email')
     nombre = request.args.get('nombre')
 
-    data = conexion.cursor()
+    cursor = conexion.cursor()
     if email:
-        data.execute("SELECT * FROM empleados WHERE correo = %s", (email,))
+        cursor.execute("SELECT * FROM empleados WHERE correo = %s", (email,))
     elif nombre:
-        data.execute("SELECT * FROM empleados WHERE nombre = %s", (nombre,))
+        cursor.execute("SELECT * FROM empleados WHERE nombre = %s", (nombre,))
     else:
         return jsonify({'error': 'No se encontró el parámetro'}), 404
 
-    empleados = data.fetchall()
-    data.close()
+    empleados = cursor.fetchall()
+    cursor.close()
     return jsonify(empleados)
 
 if __name__ == '__main__':
